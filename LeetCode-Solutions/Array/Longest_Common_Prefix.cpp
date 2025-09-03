efd24a8b4a0bd395dd2237dc59d841f56803886b
DIVYANSH-SHRIVASTAV <143244158+DIVYANSH-SHRIVASTAV@users.noreply.github.com>
2025-09-03 12:31:04 +0530

    Add Longest_Common_Prefix.cpp

--- LeetCode-Solutions/Array/Longest_Common_Prefix.cpp
+++ LeetCode-Solutions/Array/Longest_Common_Prefix.cpp
@@ -1,18 +1,2 @@
 # Solution for Longest Common Prefix in cpp
 # You can copy code manually from LeetCode
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& v) {
-        string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
-            }
-            ans+=first[i];
-        }
-        return ans;
-    }
-};
