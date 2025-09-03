d00f0b723e21eb9cb09aaff855173d4ff566bd39
DIVYANSH-SHRIVASTAV <143244158+DIVYANSH-SHRIVASTAV@users.noreply.github.com>
2025-09-03 12:48:40 +0530

    Update Valid_Parentheses.cpp

--- LeetCode-Solutions/String/Valid_Parentheses.cpp
+++ LeetCode-Solutions/String/Valid_Parentheses.cpp
@@ -1,2 +1,28 @@
 # Solution for Valid Parentheses in cpp
 # You can copy code manually from LeetCode
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>pa;
+        for(int i=0;i<s.size();i++){
+           if(s[i]=='('||s[i]=='{'||s[i]=='['){
+            pa.push(s[i]);
+           }
+           else{
+            if(pa.empty())return false;
+            else{
+               char temp= pa.top();
+               pa.pop();
+               if(temp=='('&&s[i]==')'||temp=='{'&&s[i]=='}'||temp=='['&&s[i]==']'){
+                continue;
+               }
+               else{
+                return false;
+               }
+            }
+           }
+        }
+        return pa.empty();
+    }
+};
