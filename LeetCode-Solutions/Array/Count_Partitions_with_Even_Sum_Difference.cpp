a36a6bc87547317c87068e7ff871e484940c171a
DIVYANSH-SHRIVASTAV <143244158+DIVYANSH-SHRIVASTAV@users.noreply.github.com>
2025-09-12 20:47:56 +0530

    Update Count_Partitions_with_Even_Sum_Difference.cpp

--- LeetCode-Solutions/Array/Count_Partitions_with_Even_Sum_Difference.cpp
+++ LeetCode-Solutions/Array/Count_Partitions_with_Even_Sum_Difference.cpp
@@ -1,2 +1,21 @@
 # Solution for Count Partitions with Even Sum Difference in cpp
 # You can copy code manually from LeetCode
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int tsum=0;
+        int left=0;
+        int right=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            tsum+=nums[i];
+            
+        }
+        for(int i=0;i<nums.size()-1;i++){
+            left+=nums[i];
+            right=tsum-left;
+            if((right-left)%2==0)ans++;
+        }
+        return ans;
+    }
+};
