c9b3943c167db1acb139cdcd96c94a2a3af50bc0
DIVYANSH-SHRIVASTAV <143244158+DIVYANSH-SHRIVASTAV@users.noreply.github.com>
2025-09-12 20:49:35 +0530

    Update Check_Divisibility_by_Digit_Sum_and_Product.cpp

--- LeetCode-Solutions/Math/Check_Divisibility_by_Digit_Sum_and_Product.cpp
+++ LeetCode-Solutions/Math/Check_Divisibility_by_Digit_Sum_and_Product.cpp
@@ -1,2 +1,16 @@
 # Solution for Check Divisibility by Digit Sum and Product in cpp
 # You can copy code manually from LeetCode
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum=0;
+        int mul=1;
+        int t=n;
+        while(n!=0){
+           sum+= n%10;
+            mul*=n%10;
+            n/=10;
+        }
+        return t%(sum+mul)==0;
+    }
+};
