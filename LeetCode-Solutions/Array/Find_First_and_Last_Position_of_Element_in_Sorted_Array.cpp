7d981f7351df0ad8bf76dfa34422a49665dc081c
DIVYANSH-SHRIVASTAV <143244158+DIVYANSH-SHRIVASTAV@users.noreply.github.com>
2025-09-12 20:43:32 +0530

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp

--- LeetCode-Solutions/Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ LeetCode-Solutions/Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -1,2 +1,51 @@
 # Solution for Find First and Last Position of Element in Sorted Array in cpp
 # You can copy code manually from LeetCode
+class Solution {
+    int lowerBound(vector<int>& arr, int target){
+        int s=0;
+        int e=arr.size()-1;
+        int ans = -1;
+
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(target == arr[mid]){
+                ans = mid;
+                e = mid - 1;
+            }
+            else if(target < arr[mid]){
+                e= mid -1;
+            }
+            else{
+                s=mid + 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+
+    int upperBound(vector<int>& arr, int target){
+        int s = 0;
+        int e = arr.size()-1;
+        int ans = -1;
+
+        int  mid = s + (e-s)/2;
+        while(s<=e){
+            if(target == arr[mid]){
+                ans = mid;
+                s = mid + 1;
+            }
+            else if(target < arr[mid]){
+                e= mid -1;
+            }
+            else{
+                s=mid + 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return{lowerBound(nums,target),upperBound(nums,target)};
+    }
+};
